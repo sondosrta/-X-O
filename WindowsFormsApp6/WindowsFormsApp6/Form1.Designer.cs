4e55017e8a391c0507baf13703b84f1469c5c23b
sondosrta <161839520+sondosrta@users.noreply.github.com>
2024-05-12 15:24:57 +0300

    Add files via upload

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.player1 = new System.Windows.Forms.Label();
+            this.X = new System.Windows.Forms.Label();
+            this.d = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.O = new System.Windows.Forms.Label();
+            this.player2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.a1.Location = new System.Drawing.Point(38, 40);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(75, 75);
+            this.a1.TabIndex = 0;
+            this.a1.TabStop = false;
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.btns_Click);
+            this.a1.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.a1.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.a2.Location = new System.Drawing.Point(119, 40);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(75, 75);
+            this.a2.TabIndex = 1;
+            this.a2.TabStop = false;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.btns_Click);
+            this.a2.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.a2.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.a3.Location = new System.Drawing.Point(200, 40);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(75, 75);
+            this.a3.TabIndex = 2;
+            this.a3.TabStop = false;
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.btns_Click);
+            this.a3.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.a3.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.b3.Location = new System.Drawing.Point(200, 121);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 75);
+            this.b3.TabIndex = 5;
+            this.b3.TabStop = false;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.btns_Click);
+            this.b3.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.b3.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.b2.Location = new System.Drawing.Point(119, 121);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 75);
+            this.b2.TabIndex = 4;
+            this.b2.TabStop = false;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.btns_Click);
+            this.b2.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.b2.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.b1.Location = new System.Drawing.Point(38, 121);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 75);
+            this.b1.TabIndex = 3;
+            this.b1.TabStop = false;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.btns_Click);
+            this.b1.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.b1.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.c3.Location = new System.Drawing.Point(200, 202);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(75, 75);
+            this.c3.TabIndex = 8;
+            this.c3.TabStop = false;
+            this.c3.UseVisualStyleBackColor = false;
+            this.c3.Click += new System.EventHandler(this.btns_Click);
+            this.c3.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.c3.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // c2
+            // 
+            this.c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.c2.Location = new System.Drawing.Point(119, 202);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(75, 75);
+            this.c2.TabIndex = 7;
+            this.c2.TabStop = false;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.btns_Click);
+            this.c2.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.c2.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.c1.Location = new System.Drawing.Point(38, 202);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(75, 75);
+            this.c1.TabIndex = 6;
+            this.c1.TabStop = false;
+            this.c1.UseVisualStyleBackColor = false;
+            this.c1.Click += new System.EventHandler(this.btns_Click);
+            this.c1.MouseEnter += new System.EventHandler(this.c3_MouseEnter);
+            this.c1.MouseLeave += new System.EventHandler(this.c3_MouseLeave);
+            // 
+            // player1
+            // 
+            this.player1.AutoSize = true;
+            this.player1.Location = new System.Drawing.Point(35, 290);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(53, 16);
+            this.player1.TabIndex = 9;
+            this.player1.Text = "Player1";
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.Location = new System.Drawing.Point(45, 318);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(14, 16);
+            this.X.TabIndex = 10;
+            this.X.Text = "0";
+            // 
+            // d
+            // 
+            this.d.AutoSize = true;
+            this.d.Location = new System.Drawing.Point(126, 318);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(14, 16);
+            this.d.TabIndex = 12;
+            this.d.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Draw";
+            // 
+            // O
+            // 
+            this.O.AutoSize = true;
+            this.O.Location = new System.Drawing.Point(207, 318);
+            this.O.Name = "O";
+            this.O.Size = new System.Drawing.Size(14, 16);
+            this.O.TabIndex = 14;
+            this.O.Text = "0";
+            this.O.Click += new System.EventHandler(this.O_Click);
+            // 
+            // player2
+            // 
+            this.player2.AutoSize = true;
+            this.player2.Location = new System.Drawing.Point(197, 290);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(53, 16);
+            this.player2.TabIndex = 13;
+            this.player2.Text = "Player2";
+            this.player2.Click += new System.EventHandler(this.player2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(322, 28);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.fileToolStripMenuItem.Text = "file..";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 356);
+            this.Controls.Add(this.O);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.d);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(340, 403);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(340, 403);
+            this.Name = "Form1";
+            this.Text = "X/O";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Label player1;
+        private System.Windows.Forms.Label X;
+        private System.Windows.Forms.Label d;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label O;
+        private System.Windows.Forms.Label player2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
